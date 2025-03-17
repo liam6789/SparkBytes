4a5c9e8a2ee2768e9e124f3f5d2354651f2491f2
pattyhuang <pattyh@bu.edu>
2025-03-17 19:31:00 -0400

    used app router to link the pages, implemented about page with short explanation and creator blurb

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -7,20 +7,10 @@ const { Content, Footer } = Layout;
 const LayoutComponent = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
-<<<<<<< HEAD
-      <body>
-        <Layout className="layout" style={{ minHeight: "100vh" }}>
-          <CustomHeader />
-          <Content style={{ padding: "0 50px", marginTop: 0 }}>
-            <div
-              className="site-layout-content"
-              style={{ padding: 24, minHeight: 380, height: "100%" }}
-            >
-              {children}
-            </div>
-          </Content>
-          <Footer style={{ textAlign: 'center' }}>Spark! ©2025 Created by Spark!</Footer>
-        </Layout>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <CustomHeader>
+          {children}
+        </CustomHeader>
       </body>
     </html>
     
