64a87f20dbda62b9f34c951ca798ed58026a4325
liammcdonald <liammcd3.14@gmail.com>
2025-03-23 23:28:32 -0400

    Created the basic supabase setup for the database with tables for users, events, foods, and reservations. Then, connected said database to the main.py file so that it can be used for writing FastAPI routes

---
+++ supabase/migrations/20250324015318_sparkbytes_tables.sql
@@ -0,0 +1,33 @@
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    role VARCHAR(50) CHECK (role IN ('regular_user', 'event_creator')) NOT NULL DEFAULT 'regular_user'
+);
+
+CREATE TABLE events (
+    event_id SERIAL PRIMARY KEY,
+    event_name VARCHAR(255) NOT NULL,
+    description TEXT,
+    date TIMESTAMP WITH TIME ZONE,
+    creator_id INT REFERENCES users(user_id) ON DELETE SET NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    last_res_time TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE foods (
+    food_id SERIAL PRIMARY KEY,
+    food_name VARCHAR(255) NOT NULL,
+    quantity INT NOT NULL CHECK (quantity >= 0),
+    event_id INT REFERENCES events(event_id) ON DELETE CASCADE
+);
+
+CREATE TABLE reservations (
+    res_id SERIAL PRIMARY KEY,
+    food_id INT REFERENCES foods(food_id) ON DELETE CASCADE,
+    user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
+    event_id INT REFERENCES events(event_id) ON DELETE CASCADE,
+    quantity INT NOT NULL CHECK (quantity >= 0),
+    res_time TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    notes TEXT
+);
