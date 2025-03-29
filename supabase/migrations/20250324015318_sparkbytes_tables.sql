4aa8c73f02506220b3e267da5f82b35d3ebeb020
liammcdonald <liammcd3.14@gmail.com>
2025-03-29 17:37:24 -0400

    Created a few more types specific to event creation. Now need to actually implement the backend routes involved with adding the events to our database and to also add food items associated with this new event.

--- supabase/migrations/20250324015318_sparkbytes_tables.sql
+++ supabase/migrations/20250324015318_sparkbytes_tables.sql
@@ -1,11 +1,11 @@
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     user_id SERIAL PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
     password TEXT NOT NULL,
     role VARCHAR(50) CHECK (role IN ('regular_user', 'event_creator')) NOT NULL DEFAULT 'regular_user'
 );
 
-CREATE TABLE events (
+CREATE TABLE IF NOT EXISTS events (
     event_id SERIAL PRIMARY KEY,
     event_name VARCHAR(255) NOT NULL,
     description TEXT,
@@ -15,14 +15,14 @@ CREATE TABLE events (
     last_res_time TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE foods (
+CREATE TABLE IF NOT EXISTS foods (
     food_id SERIAL PRIMARY KEY,
     food_name VARCHAR(255) NOT NULL,
     quantity INT NOT NULL CHECK (quantity >= 0),
     event_id INT REFERENCES events(event_id) ON DELETE CASCADE
 );
 
-CREATE TABLE reservations (
+CREATE TABLE IF NOT EXISTS reservations (
     res_id SERIAL PRIMARY KEY,
     food_id INT REFERENCES foods(food_id) ON DELETE CASCADE,
     user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
