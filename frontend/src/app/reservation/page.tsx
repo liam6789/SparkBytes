e578f51d75c660671117d8609a30aedc30c097f8
liammcdonald <liammcd3.14@gmail.com>
2025-04-04 16:34:27 -0400

    Created the backend functionality such that active event information is loaded into the reservation creation page on load, as well as updating the food options, and quantity options based on the event and food selected. Now I just need to implement the final backend logic that actually deals with form submission so that reservations can be added to the database and food quantity tables are updated

--- frontend/src/app/reservation/page.tsx
+++ frontend/src/app/reservation/page.tsx
@@ -1,22 +1,27 @@
 "use client";
 
-import { Typography, Button, Input, Dropdown, Menu } from "antd";
+import { Typography, Button, Input, Dropdown, Menu, TimePicker } from "antd";
 import React, { useEffect, useState } from "react";
 import { DownOutlined } from "@ant-design/icons";
 import dayjs from "dayjs";
+import ReservationTimePicker from "../components/picktime";
 import { ReservationCreate, ReservationData, FoodData, EventData } from "@/types/types";
 
 export default function ReservationPage() {
     const [eventOpts, setEventOpts] = useState<EventData[]>([]);
     const [foodOpts, setFoodOpts] = useState<FoodData[]>([]);
-    const [timeOpts, setTimeOpts] = useState<dayjs.Dayjs[]>([]);
+    const [quantityOpts, setQuantityOpts] = useState<number[]>([]);
+    const [lastTime, setLastTime] = useState<dayjs.Dayjs>(dayjs());
+    
+    const [noEvents, setNoEvents] = useState("");
+    const [noFood, setNoFood] = useState("");
 
     const [event, setEvent] = useState<EventData | null>(null);
     const [food, setFood] = useState<FoodData | null>(null);
     const [quantity, setQuantity] = useState(0);
-    const [quantityStr, setQuantityStr] = useState("");
     const [selectedTime, setSelectedTime] = useState<string | null>(null);
     const [note, setNote] = useState("");
+    const [onStart, setOnStart] = useState(true);
 
     const [submit, setSubmit] = useState(false)
 
@@ -28,38 +33,86 @@ export default function ReservationPage() {
     useEffect(() => {
         // TODO: Implement backend functionality that will load the current events into the eventOpts state so that users can select
         // events they want to make a reservation for. This should be updated on load
+        const fetchEvents = async () => {
+            const res = await fetch(`http://localhost:5001/active-events`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            })
+
+            if (res.ok) {
+                const data = await res.json();
+                console.log(data)
+                if (data.events.length > 0) {
+                    setEventOpts(data.events);
+                    setNoEvents("");
+                } else {
+                    setNoEvents("No events available. Please check back later or refresh the page.");
+                }
+            }
+        }
+
+        fetchEvents();
     }, [])
 
     useEffect(() => {
         // TODO: Implement backend functionality that will load the food options for the selected event. Updated whenever the selected event changes
-        generateTimeSlots();
+        const fetchFood = async () => {
+            const res = await fetch(`http://localhost:5001/get-food/${event?.event_id}`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            })
+
+            if (res.ok) {
+                const data = await res.json();
+                console.log(data.foods)
+                if (data.foods.length > 0) {
+                    setFoodOpts(data.foods);
+                    setNoFood("");
+                } else {
+                    setNoFood("No food options available. Please check back later or refresh the page.");
+                }
+            }
+        }
+        if (onStart) {
+            setOnStart(false);
+        } else if (event != null) {
+            fetchFood();
+            setLastTime(dayjs(event.last_res_time));
+            setFood(null);
+            setQuantity(0);
+            setQuantityOpts([]);
+            setSelectedTime(null);
+        }
     }, [event])
 
     useEffect(() => {
         // TODO: Implement backend functionality that will load the max quantity of the selected food option that is available for the 
         // selected event so that they don't enter a value greater than the max.
-    }, [food])
-
-
-    const generateTimeSlots = () => {
-        let now = dayjs();
-        let roundedNow = now.subtract(now.minute() % 5, "minute");
-        let times = [];
-
-        while (roundedNow.isBefore(event?.last_res_time)) {
-            times.push(roundedNow);
-            roundedNow = roundedNow.add(5, "minute");
+        const setOpts = (foodItem: FoodData) => {
+            let opts = [];
+            for (let i = 1; i <= foodItem.quantity; i++) {
+                opts.push(i);
+            }
+            setQuantityOpts(opts);
         }
 
-        setTimeOpts(times);
-    }
+        if (onStart) {
+            setOnStart(false);
+        } else if (food != null) {
+            setOpts(food);
+        }
+    }, [food])
 
     return (
         <>
             <Typography.Title level={1}>
                 Create Reservation
             </Typography.Title>
-
+            {noEvents && <Typography.Title level={3} type="danger">{noEvents}</Typography.Title>}
             <Typography.Title level={3}>
                 Select Event
             </Typography.Title>
@@ -76,7 +129,8 @@ export default function ReservationPage() {
                     {event?.event_name ? event.event_name : "Choose an event"} <DownOutlined />
                 </Button>
             </Dropdown>
-
+            
+            {noFood && <Typography.Title level={3} type="danger">{noFood}</Typography.Title>}
             {event != null ?
             <><Typography.Title level={3}>
                 Select Food Item
@@ -100,34 +154,36 @@ export default function ReservationPage() {
             <><Typography.Title level={3}>
                 Input Quantity
             </Typography.Title>
-            <Input
-                value={quantityStr}
-                onChange={(e) => {
-                    setQuantity(parseInt(e.target.value))
-                    setQuantityStr(e.target.value)
+            <Dropdown 
+                menu={{
+                    items: quantityOpts.map((num) => ({
+                        key: num,
+                        label: num,
+                        onClick: () => setQuantity(num),
+                    })),
                 }}
             >
-            </Input> </>: null   
+                <Button>
+                    {quantity != 0 ? quantity : "Select a Quantity"} <DownOutlined />
+                </Button>
+            </Dropdown> </>: null   
             }
 
             {event != null && food != null ? 
             <><Typography.Title level={3}>
                 Pickup Time
             </Typography.Title>
-            <Dropdown
-                menu={{
-                items: timeOpts.map((time) => ({
-                    key: time.format("HH:mm"),
-                    label: time.format("hh:mm A"),
-                    onClick: () => setSelectedTime(time.format("hh:mm A")),
-                })),
+            <ReservationTimePicker
+                lastRes={dayjs(event.last_res_time)}
+                onChange={(time) => {
+                    if (time != null) {
+                        setSelectedTime(time.format("HH:mm"));
+                    } else {
+                        setSelectedTime(null);
+                    }
                 }}
             >
-                <Button>
-                    {selectedTime || "Choose a time"} <DownOutlined />
-                </Button>
-            </Dropdown></> : null
-            }
+            </ReservationTimePicker></> : null}
 
             {selectedTime != null ? 
             <><Typography.Title level={3}>
