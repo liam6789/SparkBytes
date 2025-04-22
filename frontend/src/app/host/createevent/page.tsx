2323a36c2a8a63431cd94913031ea5f0f6c4abca
liammcdonald <liammcd3.14@gmail.com>
2025-04-21 22:50:04 -0400

    Added location data to the event details page for both hosts and regular users. Also, implemented editing abilities within the host pov for location that is based on what I did within the event creation page for locations

--- frontend/src/app/host/createevent/page.tsx
+++ frontend/src/app/host/createevent/page.tsx
@@ -156,28 +156,27 @@ export default function EventCreationPage() {
                 Event Location
             </Typography.Title>
             
-            <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!} libraries={["places"]}>
-                <Autocomplete
-                    onLoad={onLoad}
-                    onPlaceChanged={onPlaceChanged}
-                >
-                    <Input placeholder="Search for a location" value={locationStr} onChange={(e) => setLocationStr(e.target.value)}/>
-                </Autocomplete>
+            
+            <Autocomplete
+                onLoad={onLoad}
+                onPlaceChanged={onPlaceChanged}
+            >
+                <Input placeholder="Search for a location" value={locationStr} onChange={(e) => setLocationStr(e.target.value)}/>
+            </Autocomplete>
+            {location && (
+                <div style={{ marginTop: "16px", marginBottom: "16px" }}>
+                    <strong>Selected Location:</strong> {location.address}
+                </div>
+            )}
+            <GoogleMap
+                mapContainerStyle={mapStyle}
+                center={defaultCenter}
+                zoom={15}
+            >
                 {location && (
-                    <div style={{ marginTop: "16px", marginBottom: "16px" }}>
-                        <strong>Selected Location:</strong> {location.address}
-                    </div>
+                    <Marker position={{ lat: location.lat, lng: location.lng }} />
                 )}
-                <GoogleMap
-                    mapContainerStyle={mapStyle}
-                    center={defaultCenter}
-                    zoom={15}
-                >
-                    {location && (
-                        <Marker position={{ lat: location.lat, lng: location.lng }} />
-                    )}
-                </GoogleMap>
-            </LoadScript>
+            </GoogleMap>
 
             <Typography.Title level={2}>
                 Add Food Items
